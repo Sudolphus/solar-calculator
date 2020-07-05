d3fb385fdd0ec482e60c4435dfb1247a5fd02f79
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-05 15:24:23 -0700

    seperate the logic

---
+++ src/display-control.js
@@ -0,0 +1,28 @@
+import { SolarExpectancy } from './solar-expectancy';
+
+function buildFormHTML(planetArray, ageArray, expectationArray, leftToLiveArray) {
+  let formHTML = '';
+  for (let i = 0; i < ageArray.length; i++) {
+    formHTML += `<li>Your age in ${planetArray[i+1]} years is ${ageArray[i]} years!</li>`;
+  }
+  if (leftToLiveArray[0] >= 0) {
+    for (let i = 0; i < expectationArray.length; i++) {
+      formHTML += `<li>Your life expectancy in ${planetArray[i]} years is ${expectationArray[i]}! That means you have ${leftToLiveArray[i]} left to live!`;
+    }
+  } else {
+    for (let i = 0; i < expectationArray.length; i++) {
+      formHTML += `<li>Your life expectancy in ${planetArray[i]} years is ${expectationArray[i]}! That means you are ${leftToLiveArray[i]*-1} years over budget!`;
+    }
+  }
+  return formHTML;
+}
+
+export function displaySolarValues(formOutput, userAge, userGender, userContinent) {
+  let solar = new SolarExpectancy(userAge, userGender, userContinent);
+  const planetArray = ["Earth", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+  const ageArray = solar.ageConversionArray(solar.userAge);
+  const expectationArray = solar.ageExpectancyArray();
+  const leftToLiveArray = solar.leftToLiveArray();
+  const formHTML = buildFormHTML(planetArray, ageArray, expectationArray, leftToLiveArray);
+  formOutput.html(formHTML);
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jQuery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { SolarExpectancy } from './solar-expectancy';
+import { displaySolarValues } from './display-control';
 
 function gatherUserInput() {
   const userAge = parseFloat($('input[name="ageInput"]').val()).toFixed(2);
@@ -11,39 +11,12 @@ function gatherUserInput() {
   return [userAge, userGender, userContinent];
 }
 
-function buildFormHTML(planetArray, ageArray, expectationArray, leftToLiveArray) {
-  let formHTML = '';
-  for (let i = 1; i < ageArray.length; i++) {
-    formHTML += `<li>Your age in ${planetArray[i+1]} years is ${ageArray[i]} years!</li>`;
-  }
-  if (leftToLiveArray[0] >= 0) {
-    for (let i = 0; i < expectationArray.length; i++) {
-      formHTML += `<li>Your life expectancy in ${planetArray[i]} years is ${expectationArray[i]}! That means you have ${leftToLiveArray[i]} left to live!`;
-    }
-  } else {
-    for (let i = 0; i < expectationArray.length; i++) {
-      formHTML += `<li>Your life expectancy in ${planetArray[i]} years is ${expectationArray[i]}! That means you are ${leftToLiveArray[i]*-1} years over budget!`;
-    }
-  }
-  return formHTML;
-}
-
-function displaySolarValues(solar) {
-  const formOutput = $('#formOutput');
-  const planetArray = ["Earth", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
-  const ageArray = solar.ageConversionArray(solar.userAge);
-  const expectationArray = solar.ageExpectancyArray();
-  const leftToLiveArray = solar.leftToLiveArray();
-  const formHTML = buildFormHTML(planetArray, ageArray, expectationArray, leftToLiveArray);
-  formOutput.html(formHTML);
-}
-
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault();
-    $('#formOutput').empty();
+    const formOutput = $('#formOutput');
+    formOutput.empty();
     const [userAge, userGender, userContinent] = gatherUserInput();
-    let solar = new SolarExpectancy(userAge, userGender, userContinent);
-    displaySolarValues(solar);
+    displaySolarValues(formOutput, userAge, userGender, userContinent);
   });
 });
