baaca4eeecf46c534d049767208de2b6784ff69b
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-05 14:42:33 -0700

    add basic ui

--- src/main.js
+++ src/main.js
@@ -2,3 +2,18 @@ import $ from 'jQuery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+function gatherUserInput() {
+  const userAge = parseFloat($('input[name="ageInput"]').val()).toFixed(2);
+  const userGender = $('[name="genderSelect"]').val();
+  return [userAge, userGender];
+}
+
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    const [userAge, userGender] = gatherUserInput();
+    alert(userAge);
+    alert(userGender);
+  });
+});
