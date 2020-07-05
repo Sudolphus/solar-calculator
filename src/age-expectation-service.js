fa18cd60596faa4d36e646667fd5d84974ccd9e3
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-05 13:16:32 -0700

    add method for calculating age expectancy

--- src/age-expectation-service.js
+++ src/age-expectation-service.js
@@ -1,5 +1,28 @@
 export class AgeExpectationService {
-  ageExpectation() {
-    
+  ageExpectation(userGender, userContinent) {
+    let userExpectation;
+    switch (userContinent) {
+    case ('north america'):
+      userExpectation = 76;
+      break;
+    case ('oceania'):
+      userExpectation = 76;
+      break;
+    case ('europe'):
+      userExpectation = 75;
+      break;
+    case ('south america'):
+      userExpectation = 72;
+      break;
+    case ('asia'):
+      userExpectation = 71;
+      break;
+    case ('africa'):
+      userExpectation = 61;
+    }
+    if (userGender === 'female') {
+      userExpectation += 5;
+    }
+    return userExpectation;
   }
 }
